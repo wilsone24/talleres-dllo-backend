632059b18efea32b7eea2a25e114dcca6a2ef8fd
wilsone24 <wilsone@uninorte.edu.co>
2025-08-23 11:37:34 -0500

    Add functions for findMax, includes, sum, and missingNumbers in taller-02.js

---
+++ taller-02.js
@@ -0,0 +1,65 @@
+/*
+Punto#1 findMax
+Desarrolle una funcion llamada findMax que reciba una lista de números porparámetro y retorne el mayor valor.
+*/
+
+const findMax = function (numeros) {
+  if (numeros.length === 0) {
+    return null;
+  }
+  let max = numeros[0];
+  for (let i = 1; i < numeros.length; i++) {
+    if (numeros[i] > max) {
+      max = numeros[i];
+    }
+  }
+  return max;
+};
+
+/*
+Punto#2 includes
+Desarrolle una funcion llamada includes que reciba una lista de números y un numero por parámetro y retorne un booleano representando si el numero se encuentra en la lista.
+*/
+
+const includes = function (numeros, numero) {
+  for (let i = 0; i < numeros.length; i++) {
+    if (numeros[i] === numero) {
+      return true;
+    }
+  }
+  return false;
+};
+
+/*
+Punto#3 sum
+Desarrolle una funcion llamada sum que reciba una lista de números y retorne la suma de los elementos de la lista.
+*/
+
+const sum = function (numeros) {
+  let total = 0;
+  for (let i = 0; i < numeros.length; i++) {
+    total += numeros[i];
+  }
+  return total;
+};
+
+/*
+Punto#4 missingNumbers
+Desarrolle una funcion llamada missingNumbers que reciba una lista de númerosy retorne una lista de los números faltantes entre el menor y mayor de la lista.
+
+*/
+
+const missingNumbers = function (numeros) {
+  if (numeros.length === 0) {
+    return [];
+  }
+  const min = findMin(numeros);
+  const max = findMax(numeros);
+  const missing = [];
+  for (let i = min; i <= max; i++) {
+    if (!includes(numeros, i)) {
+      missing[missing.length] = i;
+    }
+  }
+  return missing;
+};
