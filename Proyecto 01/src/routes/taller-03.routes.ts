65dafb437f6cdad2ee8c550dd88e8e72f00ff34b
wilsone24 <wilsone@uninorte.edu.co>
2025-11-03 17:21:36 -0500

    feat: Implement route handlers for desglosarString, twoSum, conversionRomana, and descomposicion in taller-03.routes.ts

--- Proyecto 01/src/routes/taller-03.routes.ts
+++ Proyecto 01/src/routes/taller-03.routes.ts
@@ -9,10 +9,30 @@ import {
 
 const taller03Routes = Router();
 
-const postDesglosarString = (request: Request, response: Response): void => {};
-const postTwoSum = (request: Request, response: Response): void => {};
-const postConversionRomana = (request: Request, response: Response): void => {};
-const postDescomposicion = (request: Request, response: Response): void => {};
+const postDesglosarString = (request: Request, response: Response): void => {
+  const {
+    string,
+    string2,
+  }: { string: string; string2: "vocales" | "consonantes" } = request.body;
+  const resultado = desglosarString(string, string2);
+  response.status(200).json({ resultado });
+};
+const postTwoSum = (request: Request, response: Response): void => {
+  const { numeros, numero }: { numeros: number[]; numero: number } =
+    request.body;
+  const resultado = twoSum(numeros, numero);
+  response.status(200).json({ resultado });
+};
+const postConversionRomana = (request: Request, response: Response): void => {
+  const { romano }: { romano: string } = request.body;
+  const resultado = conversionRomana(romano);
+  response.status(200).json({ resultado });
+};
+const postDescomposicion = (request: Request, response: Response): void => {
+  const { string }: { string: string } = request.body;
+  const resultado = descomposicion(string);
+  response.status(200).json({ resultado });
+};
 
 taller03Routes.post("/desglosarString", postDesglosarString);
 taller03Routes.post("/twoSum", postTwoSum);
